25b5bbdd5c47c6f597e3d3476777e34c70f4a193
B Ranadeer <davidbasiripogu@hotmail.com>
2025-09-21 18:55:02 +0530

    initial

---
+++ project.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly as pl
+import matplotlib as plt
+
+
+def main():
+    ...
+
+
+def response():
+    ...
+
+
+def mathvalue():
+    ...
+
+
+def truth():
+    ...
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_project.py
@@ -0,0 +1,20 @@
+import pytest
+from project import response, mathvalue, truth
+
+
+def main():
+    test_reponse()
+    test_mathvalue()
+    test_truth()
+
+
+def test_reponse():
+    ...
+
+
+def test_mathvalue():
+    ...
+
+
+def test_truth():
+    ...
